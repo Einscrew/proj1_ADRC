fa45ec9709c78ca90ed7f0f0958de815927fbdd5
Einscrew <manel.rodrigo.reis@gmail.com>
2017-09-27 00:16:56 +0100

    Return node elements functions

--- src/trees.c
+++ src/trees.c
@@ -9,6 +9,38 @@ Node *allocNode(){
 	return ((Node *) malloc(sizeof(Node)));
 }
 
+Node *getRight(Node * node){
+	return node->right;
+}
+
+Node *getLeft(Node * node){
+	return node->left;
+}
+
+int getValue(Node * node){
+	return node->value;
+}
+
+void setRight(Node * node, int num){
+	if(node->right == NULL)
+		node->right=newNode(num);
+	else
+		setValue(&node->right, num);
+}
+
+void setLeft(Node * node, int num){
+	if(node->left == NULL)
+		node->left=newNode(num);
+	else
+		setValue(&node->left, num);
+}
+void setValue(Node ** node, int num){
+	(*node)->value = num;
+}
+
+
+
+
 Node *newNode(int num){
 	
 	Node* aux;
--- src/trees.h
+++ src/trees.h
@@ -13,6 +13,12 @@ typedef struct _Node Node;
 
 Node *allocNode();
 Node *newNode(int num);
+Node *getRight(Node * node);
+Node *getLeft(Node * node);
+int getValue(Node * node);
+void setRight(Node * node, int num);
+void setLeft(Node * node, int num);
+void setValue(Node ** node, int num);
 void freeTree(Node * root);
 
 #endif
