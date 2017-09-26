292c53ca3d667883082f418f9ee56e6c51e64219
Einscrew <manel.rodrigo.reis@gmail.com>
2017-09-27 00:18:50 +0100

    Corrections, insertNode(), rudimental print()

--- src/prefixTree.c
+++ src/prefixTree.c
@@ -1,28 +1,69 @@
-#include "stdlib"
-#include "stdio.h"
-#include "trees.h"
 #include "prefixTree.h"
+
+#define FICH "t.txt"
+#define PREF_MAX_SIZE 16
+#define LINE 20
+
+void insertNode(char* p, int nextHop, int height, Node * node, int i){
+	if(height > 0){
+		if(p[i] == '1'){
+			if(getRight(node) == NULL)
+				setRight(node, -1);
+
+			insertNode(p, nextHop, --height, (Node *)getRight(node),++i);
+			
+		}
+		else if(p[i]=='0'){
+			if(getLeft(node) == NULL)
+				setLeft(node, -1);
+
+			insertNode(p, nextHop, --height, (Node *)getLeft(node),++i);
+			
+		}
+	}
+	else{
+		setValue(&node, nextHop);
+	}
+}
 
 Node* PrefixTree(){
 
-	FILE* ptr = fopen("nome_do_ficheiro", "r");
-	char line[20];
-	char prefix[16];
+	FILE* ptr = fopen(FICH, "r");
+	char line[LINE];
+	char prefix[PREF_MAX_SIZE];
 	int nextHop = 0;
 	Node *root = newNode(-1);
 
 	if(ptr == NULL)
 	{
 		printf("There isn't any file");
-		exit(-1);
+//------------exit(-1);
+		return NULL;
 	}
 
-	while((line = fgets(line, 20, "nome_do_ficheiro")) != EOF)
-	{
-		sscanf(line, "%s %d", prefix, nextHop);
-		//insertNode(prefix, nextHop);
+	while(fgets(line, 20, ptr)!= NULL){
+	
+		sscanf(line, "%s %d", prefix, &nextHop);
+		printf("\t\t%s----%d\n", prefix, nextHop);
+		insertNode(prefix, nextHop, strlen(prefix), root, 0);
 	}
+	//printf("%d || %d\n",getValue(getLeft(root)), getValue(getLeft(getLeft(root))) );
+	printTree(root);
 
 	return root;
 
+}
+
+void printTree(Node * node){
+	if(getLeft(node) == NULL){
+		printf("%d\n", getValue(node));
+	}
+	else
+		printTree(getLeft(node));
+
+	if(getRight(node) == NULL){
+		printf("%d\n", getValue(node));
+	}
+	else
+		printTree(getRight(node));
 }
--- src/prefixTree.h
+++ src/prefixTree.h
@@ -1,4 +1,11 @@
+#include <string.h>
+#include "trees.h"
 
+#ifndef PREF
+#define  PREF
 
-
+void insertNode(char * ptrPref, int nextHop, int height, Node * node, int i);
 Node* PrefixTree();
+void printTree(Node * node);
+
+#endif
