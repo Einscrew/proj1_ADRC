144ca4902ab886ad974fe92e74b0c509486427c7
catarinaacfa <catariinaacfa@gmail.com>
2017-09-27 13:19:37 +0100

    another warning

--- src/prefixTree.h
+++ src/prefixTree.h
@@ -6,6 +6,8 @@
 
 #define NO_HOPE -1
 
+Node* PrefixTree();
+
 //void insertNode(char * ptrPref, int nextHop, int height, Node * node, int i);
 void insertNode(Node *node, char prefix[16], int nextHop, int prefixLength, int number);
 void printTree(Node * node);
