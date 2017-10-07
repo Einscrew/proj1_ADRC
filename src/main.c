732c3f96fdf96bb4a1b8a40bf77baa50d36c0473
catarinaacfa <catariinaacfa@gmail.com>
2017-10-07 23:09:23 +0100

    report added

--- src/main.c
+++ src/main.c
@@ -1,3 +1,18 @@
+/****************************************************************************************************
+*
+* This is a program that receives a file as an input with a prefix table. That table is represented 
+* by a prefix tree.
+* Users can perform many tasks, such as: 
+* 		- printing the prefix table to the screen;
+*		- inserting a new prefix and the associated next-hop;
+*		- looking for the next-hop of an address;
+*		- deleting a prefix from the tree.
+*
+* Authors: Manuel Reis (81074)
+*		   Catarina Aleixo (81731)
+*
+*****************************************************************************************************/
+
 #include "prefixTree.h"
 
 int main(int argc, char const *argv[])
